71db6ff7d74723c786c1c69aa5570a45d3629a8b
PhantasmicDev <oscar.phantasmic@protonmail.com>
2023-07-01 23:34:34 -0700

    placeholder scripts to avoid logs in console

---
+++ Editor/PackageTemplateEditor.cs
@@ -0,0 +1,10 @@
+using UnityEditor;
+
+namespace PhantasmicDev.PackageTemplate.Editor
+{
+    [CustomEditor(typeof(PackageTemplate))]
+    public class PackageTemplateEditor : UnityEditor.Editor
+    {
+        
+    }
+}
---
+++ Tests/Editor/PlaceholderTests.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace PhantasmicDev.PackageTemplate.Tests.Editor
+{
+    public class PlaceholderTests
+    {
+        // A Test behaves as an ordinary method
+        [Test]
+        public void PlaceholderTestsSimplePasses()
+        {
+            // Use the Assert class to test conditions
+        }
+    }
+}
